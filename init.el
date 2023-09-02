c474c45ee29efb9715fabec5a9a483f2a964a4a5
bitestring <81476430+bitestring@users.noreply.github.com>
2023-09-02 08:54:52 +0000

    Customize font size and weight

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
  '(display-time-mode t)
  '(global-display-line-numbers-mode t)
  '(package-selected-packages
-   '(rust-mode company lsp-haskell which-key dap-mode lsp-treemacs lsp-ivy helm-lsp lsp-ui lsp-mode treemacs-tab-bar unicode-fonts haskell-mode fullframe flycheck treemacs-tab-bar treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs projectile magit use-package))
+   '(dap-rust dap-cpptools rust-mode company lsp-haskell which-key dap-mode lsp-treemacs lsp-ivy helm-lsp lsp-ui lsp-mode treemacs-tab-bar unicode-fonts haskell-mode fullframe flycheck treemacs-tab-bar treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs projectile magit use-package))
  '(size-indication-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -24,7 +24,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 143 :width normal)))))
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight medium :height 151 :width normal)))))
 
 
 (require 'package)
