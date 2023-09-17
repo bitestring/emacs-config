30bb3c28de2def43bee94c883ad7d551a3730a40
bitestring <81476430+bitestring@users.noreply.github.com>
2023-09-17 13:30:06 +0000

    Add Nix mode

--- init.el
+++ init.el
@@ -145,6 +145,19 @@
 
 ;; language servers
 
+;; Nix
+(use-package nix-mode
+  :mode ("\\.nix\\'" "\\.nix.in\\'"))
+(use-package nix-drv-mode
+  :ensure nix-mode
+  :mode "\\.drv\\'")
+(use-package nix-shell
+  :ensure nix-mode
+  :commands (nix-shell-unpack nix-shell-configure nix-shell-build))
+(use-package nix-repl
+  :ensure nix-mode
+  :commands (nix-repl))
+
 ;; Haskell
 ;; https://bebyx.co.ua/en/log/emacs-haskell-lsp.html
 (use-package haskell-mode)
